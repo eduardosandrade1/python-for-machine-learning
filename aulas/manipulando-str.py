b5bba137c161a99c8988b9cf90d633754ae3f11a
martilian <tvduardo@gmail.com>
2020-07-17 01:04:27 -0300

    manipulando strings

---
+++ aulas/manipulando-str.py
@@ -0,0 +1,17 @@
+frase = 'Estou aprendendo ML'
+
+print(frase[6:17])
+print(frase[5:])
+print(frase[:17])
+print(frase[3:17:2])
+
+#contador de string
+print(frase.count('o'))
+
+#comprimento da frase
+
+print(len(frase))
+
+#substituição de palavras
+
+print(frase.replace('ML','algo novo'))
